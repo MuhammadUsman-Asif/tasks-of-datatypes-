b617ba10d8d50bee94a6fa0a290fdbbe6fa27e1d
Muhammadusman-Asif <usman.asif536@gmail.com>
2025-02-08 15:32:09 +0500

    first commit

---
+++ class.py
@@ -0,0 +1,115 @@
+"""x = 45 
+y = 10
+
+sum_result = x + y
+
+subtract_result = x - y
+
+division_result = x / y
+  
+print(sum_result)
+print(subtract_result)
+print(division_result)
+test =['hello','world','this','me']
+
+student={
+    "name":"talha"
+}
+print(student['name'],[test])
+
+
+test =['how ','r ', 'u']
+
+student ={
+    "name":"usman"
+}
+print (student['name'],test)
+
+
+student = [
+
+    {"name":"Usman", "age":21, "course":"python language"},
+    {"name":"Zulfi", "age":55, "course":"python language"},
+    {"name":"Ejaz", "age":57, "course":"python language"}
+]
+
+print (student)
+print (student[0])
+print (student[1])
+print (student[2])
+
+#Area of circle 
+
+pie = 3.14
+radius = 9
+
+area = pie * radius ** 2
+
+print ('area of circle =',area)
+
+#Task 3
+
+# 1- write e program to swap the values of two variables
+
+x=60                 #value initiazing number
+y=25
+print ("The Variables Before swapping ",x , y) 
+temporary_variable=y #it's a temporary variable that store the value of y
+y=x                  # now we are assigning the value of x to y
+x=temporary_variable # And here we are assigning the value that hav been store in temporary variable to x
+print ("Variables after Swapping", x , y)
+
+# 2-write a program to convert temprature from celsius to Fahrenheit and vice versa.
+
+F=30
+C=25
+F=(C*9/5)+32
+print ("the value is in Fahrenheit", F)
+C=(F*5/9)-32
+print ("The value is in celsius ", C)
+
+# 3- Assign the values 10,20 and 30 to variable poqur in a single líne.
+p,q,r= 10,20,30
+print(p)
+print(q)
+print(r)
+
+# 4- Swap the values of three variables x,y,z such that x takes the value of y and y takes the value of z and z takes the value of x 
+
+x=32
+y=5
+z=17
+x,y,z=y,z,x
+print("the values after swaping is :", x ,y,z)
+
+#Task 4
+
+# 1-write a program to calculate the distance traveled by the car moving at the speed 60km/h for 2.5 hours.
+
+speed=60
+time =2.5
+distance=speed * time
+print ("The distance of car is:", distance, "km")
+
+# 2-Write a program to calculate the simple interest for a principal amount of $1000, a rate of 5% annually,
+# and a time period of 3 years.
+Principal_Amount=1000
+Rate=5
+Time=3
+Interest = Principal_Amount*Rate*Time/100
+print ("The amount of interest is :", Interest ,"$")
+
+#part 3
+a = 2002
+b = 2025
+x= b - a
+
+c = 7
+d = 2
+y = d - c
+
+e = 7
+f = 8
+z = f - e
+
+print (x,y,z)"""
